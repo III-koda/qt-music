5e8118734164c084309484f728bfc3ce45839f19
Bao Than <b.than@sfsu.edu>
2024-05-05 13:51:00 -0700

    download status display added

--- src/ui/download_song_dialog.cpp
+++ src/ui/download_song_dialog.cpp
@@ -8,12 +8,19 @@
 
 #include <QDebug>
 
+
 void
 DownloaderThread::run() {
     DownloadStatus status = (download_spotify_song(m_url, m_download_dir))
         ? DownloadStatus::DONE
         : DownloadStatus::FAILED;
     m_caller->download_finished_callback(m_url, status);
+}
+
+QString
+SongDownloadItem::generate_text_content(std::string url, DownloadStatus status) {
+    std::string status_emoji = stringify_download_status(status, true /* is emoji */);
+    return QString((status_emoji + "\t" + url).c_str());
 }
 
 DownloadSongDialog::DownloadSongDialog(QWidget *parent) : QDialog(parent),
@@ -37,7 +44,9 @@ void DownloadSongDialog::initialize_components()
             "Spotify track URL (e.g. https://open.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB)");
 
     m_download_progress_list = m_ui->download_progress_list;
-    m_download_progress_list_header = new QListWidgetItem(tr("Status\t\tURLs"),
+    std::string header_content =
+            "Status\tURL";
+    m_download_progress_list_header = new QListWidgetItem(tr(header_content.c_str()),
                                                           m_download_progress_list);
 
     m_download_song_button = m_ui->download_button;
@@ -50,34 +59,32 @@ void DownloadSongDialog::initialize_components()
 void DownloadSongDialog::download_song_button_clicked()
 {
     std::string url = m_song_url_input_box->text().toStdString();
+    if (url.empty()){
+        return;
+    }
+    SongDownloadItem* song_download_item = new SongDownloadItem(m_download_progress_list,
+                                                                url, 
+                                                                m_download_progress_list->count());
 
-    SongDownloadItem song_download_item(m_download_progress_list, 
-                                        url, 
-                                        m_download_progress_list->count());
-
-    // m_download_progress_list->addItem(song_download_item);
+    m_download_progress_list->addItem(song_download_item);
 
     DownloaderThread* downloader = new DownloaderThread(url, m_download_dir, this);
     downloader->start();
 }
 
-void DownloadSongDialog:: sample_qlistwidget() {
-    // //Initial headers
-    // std::string done = "DONE";
-    // std::string downloading = "Downloading";
-    // std::string urls = "https://open.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB";
-    // std::string itemText = downloading + "\t" + urls;
-    // m_download_progress_list->addItem(QListWidgetItem(""));
-    // m_download_progress_list->addItem(itemText.c_str());
-    // m_download_progress_list->addItem(itemText.c_str());
-    
-    // qDebug() << QString((m_download_progress_list->count()));
-}
-
 void
 DownloadSongDialog::download_finished_callback(std::string url, DownloadStatus status) {
+    // Find the corresponding SongDownloadItem instance and update status
+    // Start at index 1 since header will be at 0
+    for (unsigned int i = 1; i < m_download_progress_list->count(); i++) {
+        auto item = static_cast<SongDownloadItem*>(m_download_progress_list->item(i));
+        if (item->url() == url) {
+            item->set_status(status);
+            break;
+        }
+    }
+
     if (status == DownloadStatus::DONE) {
         m_main_window->get_notified_song_downloaded(m_download_dir);
-    } else if (status == DownloadStatus::FAILED) {
     }
 }
