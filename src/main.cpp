6261c00255b8b1d2c836929efebe0bc5f0724fdb
Luan Truong <83048295+truongluan303@users.noreply.github.com>
2024-05-08 00:32:11 -0700

    Remove commented code in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,37 +22,3 @@ int main(int argc, char *argv[]) {
     w.setFixedSize(w.size());
     return a.exec();
 }
-
-
-// #include <QApplication>
-// #include <QLabel>
-// #include <QTimer>
-// #include <iostream>
-
-// int main(int argc, char *argv[]) {
-//     QApplication a(argc, argv);
-
-//     // Create a QLabel widget
-//     std::string s = "Moving Text12345";
-
-//     QLabel label(s.c_str());
-//     label.setAlignment(Qt::AlignCenter);
-
-
-//     if (label.fontMetrics().width(label.text()) > label.width())
-//     {
-//         QTimer timer;
-//         QObject::connect(&timer, &QTimer::timeout, [&]() {
-//             // Move the text to the right
-//             QString text = label.text();
-//             text.push_back(text.front());
-//             text.remove(0, 1);
-//             label.setText(text);
-//         });
-//         timer.start(150);
-//     }
-
-//     label.show();
-
-//     return a.exec();
-// }
